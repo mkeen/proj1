30afd867edd1910d563302f0528479aa400e0415
Your Name <you@example.com>
2016-03-10 19:21:55 -0500

    blam

--- app/type/boot.ts
+++ app/type/boot.ts
@@ -1,6 +1,8 @@
 ///<reference path="../node_modules/angular2/typings/browser.d.ts"/>
 
 import {bootstrap} from 'angular2/platform/browser'
-import {Application} from './components/application'
+import {ROUTER_PROVIDERS, Location, LocationStrategy} from 'angular2/router';
+import {Application} from './components/application';
+import {provide} from 'angular2/core';
 
-bootstrap(Application);
+bootstrap(Application, [ROUTER_PROVIDERS]);
--- app/type/components/application.ts
+++ app/type/components/application.ts
@@ -1,7 +1,15 @@
 import {Component} from 'angular2/core';
+import {RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
+import {Dashboard} from './dashboard';
+import {Router} from 'angular2/router';
 
 @Component({
     selector: 'app', 
-    template: '<h1>Proj1</h1>'
+    templateUrl: '/templates/application.html',
+    directives: [ROUTER_DIRECTIVES]
 })
+@RouteConfig([
+  {path: '/', redirectTo: ['Dashboard']},
+  {path: '/dashboard', name: 'Dashboard', component: Dashboard}
+])
 export class Application { }
